937824535bd0ec3e1422ef24ec654041fba29dc6
Olajuwon47 <lawal.olajuwonwaheed@gmail.com>
2025-04-03 21:11:43 +0100

    Hello

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import {  } from 'react'
+import Nav from './Nav.tsx'
+import './index.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Nav/> 
+      
+    </>
+  )
+}
+
+export default App
