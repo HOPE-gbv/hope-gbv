0255a8d4de904aa13504aa49769d290b2c585e33
idrisabdulrahman114 <idrisabdulrahman114@gmail.com>
2025-05-12 07:55:19 +0100

    Chore 🚀: fixed route names

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import {  } from 'react';
 import Header from './Header.tsx';
 import { Routes, Route } from "react-router-dom";
 import Home  from './Pages/Home.tsx';  
-import Pro from './Pages/Pro.bono';
+import Pro from './Pages/Pro-bono.tsx';
 import Faq  from './Pages/Faq.tsx';
 import Footer  from './Footer.tsx';
 import './index.css'
@@ -14,7 +14,7 @@ function App() {
     <>
       <Header/>
        <Routes>
-         <Route path="/Home" element={<Home />} />
+         <Route path="/" element={<Home />} />
           <Route  path="/pro-bono"element= {<Pro />} />
           <Route path="/faq" element={<Faq />} />
        </Routes>
