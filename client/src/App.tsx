c2509535da29f4543f2279b0b584ff03f48f3b6a
sulaiman200 <sulaimanidris262@gmail.com>
2025-05-15 11:11:52 +0100

    added basic donation page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+/* 
 import {  } from 'react'
 import Header from './Header.tsx';
 //import Home  from './Pages/Home.tsx';  
@@ -5,6 +6,8 @@ import Header from './Header.tsx';
 import Faq  from './Pages/Faq.tsx';
 import Footer  from './Footer.tsx';
 import './index.css'
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './Pages/Home.tsx';
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -12,14 +15,43 @@ function App() {
   return (
     <>
      <Header/> 
-     {/*<Home/>
-     <Pro/>*/}
+     <BrowserRouter>
+     <Route path='/' element={<Home />} />
      <Faq/>
+     </BrowserRouter>
      <Footer/>    
     </>
   )
 }
 
 export default App
+*/
+
+
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header.tsx';
+import Footer  from './Footer.tsx';
+import Home from './Pages/Home';
+import Donate from './Pages/Donation.tsx';
+
+
+
+function App () {
+  return (
+  
+  <>
+    <Header/>
+    <BrowserRouter>
+    <Route path='/' element={<Home />} />
+    <Route path='donate' element={<Donate />} />
+      <p>hello world</p>
+      </BrowserRouter>
+      <Footer />
+      </>
+  )
+}
+export default App
+
 
 
